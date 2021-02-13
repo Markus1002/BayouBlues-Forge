f7f1378be8af33204bbaa3730e5e3b7fa9282ebf
Epic312 <kaden_ostendorf@yahoo.com>
2021-02-13 14:21:59 -0700

    honey gooseberry stuff

--- src/main/java/com/teamaurora/bayou_blues/core/registry/BayouBluesItems.java
+++ src/main/java/com/teamaurora/bayou_blues/core/registry/BayouBluesItems.java
@@ -3,10 +3,7 @@ package com.teamaurora.bayou_blues.core.registry;
 import com.minecraftabnormals.abnormals_core.core.util.registry.ItemSubRegistryHelper;
 import com.teamaurora.bayou_blues.common.item.*;
 import com.teamaurora.bayou_blues.core.BayouBlues;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Food;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 
@@ -19,6 +16,10 @@ public class BayouBluesItems {
     public static final RegistryObject<Item> GOOSEBERRIES = HELPER.createItem("gooseberries", ()->new Item(new Item.Properties().food(Foods.GOOSEBERRIES).group(ItemGroup.FOOD)));
     public static final RegistryObject<Item> GOOSEBERRY_JUICE = HELPER.createItem("gooseberry_juice", ()->new DrinkItem(new Item.Properties().food(Foods.GOOSEBERRY_JUICE).maxStackSize(16).group(ItemGroup.FOOD)));
     public static final RegistryObject<Item> GOOSEBERRY_PIE = HELPER.createItem("gooseberry_pie", ()->new Item(new Item.Properties().food(Foods.GOOSEBERRY_PIE).group(ItemGroup.FOOD)));
+
+    public static final RegistryObject<Item> HONEY_GLAZED_GOOSEBERRIES = HELPER.createItem("honey_glazed_gooseberries", ()->new Item(new Item.Properties().food(Foods.HONEY_GLAZED_GOOSEBERRIES).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> GOOSEBERRY_JAM = HELPER.createItem("gooseberry_jam", ()->new DrinkItem(new Item.Properties().containerItem(Items.GLASS_BOTTLE).food(Foods.GOOSEBERRY_JAM).maxStackSize(16).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> GOOSEBERRY_JAM_COOKIE = HELPER.createItem("gooseberry_jam_cookie", ()->new Item(new Item.Properties().food(Foods.GOOSEBERRY_JAM_COOKIE).group(ItemGroup.FOOD)));
 
     public static final RegistryObject<Item> ALGAE = HELPER.createItem("algae", ()->new AlgaeItem(BayouBluesBlocks.ALGAE.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
     public static final RegistryObject<Item> BEARD_MOSS = HELPER.createItem("beard_moss", ()->new TreeMossItem(BayouBluesBlocks.BEARD_MOSS.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
@@ -37,5 +38,9 @@ public class BayouBluesItems {
         public static final Food GOOSEBERRIES = (new Food.Builder()).hunger(2).saturation(0.2F).build();
         public static final Food GOOSEBERRY_JUICE = (new Food.Builder()).hunger(1).saturation(0.1F).build();
         public static final Food GOOSEBERRY_PIE = (new Food.Builder()).hunger(6).saturation(0.5F).build();
+
+        public static final Food HONEY_GLAZED_GOOSEBERRIES = (new Food.Builder()).hunger(7).saturation(0.3F).build();
+        public static final Food GOOSEBERRY_JAM = (new Food.Builder()).hunger(2).saturation(0.25F).build();
+        public static final Food GOOSEBERRY_JAM_COOKIE = (new Food.Builder().hunger(2).saturation(0.3F)).build();
     }
 }
