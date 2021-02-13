a2155148ab0577f2b90522ab1484cad2af101dfb
Epic312 <kaden_ostendorf@yahoo.com>
2021-02-13 14:51:25 -0700

    fishing near algae increases fishing loot luck

--- src/main/java/com/teamaurora/bayou_blues/core/other/BayouBluesEvents.java
+++ src/main/java/com/teamaurora/bayou_blues/core/other/BayouBluesEvents.java
@@ -4,15 +4,26 @@ import com.teamaurora.bayou_blues.common.block.CypressKneeBlock;
 import com.teamaurora.bayou_blues.common.block.DoubleCypressKneeBlock;
 import com.teamaurora.bayou_blues.common.block.LilyFlowerBlock;
 import com.teamaurora.bayou_blues.core.BayouBlues;
+import com.teamaurora.bayou_blues.core.registry.BayouBluesBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.item.FishingRodItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Stats;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.IWorldWriter;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -43,6 +54,38 @@ public class BayouBluesEvents {
             if (lily != null) {
                 world.setBlockState(pos, lily.getDefaultState(), 3);
                 event.setResult(Event.Result.ALLOW);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLivingEntityUseItem(LivingEntityUseItemEvent event) {
+        if (event.getItem().getItem() instanceof FishingRodItem) {
+            if (event.getEntity() instanceof PlayerEntity) {
+                PlayerEntity playerIn = (PlayerEntity) event.getEntity();
+                World worldIn = playerIn.getEntityWorld();
+                Hand handIn = playerIn.swingingHand;
+                ItemStack itemstack = event.getItem();
+                if (playerIn.fishingBobber == null) {
+                    boolean flag = false;
+                    for (BlockPos pos : BlockPos.getAllInBoxMutable(playerIn.getPosition().add(-10, -10, -10), playerIn.getPosition().add(10, 10, 10))) {
+                        if (worldIn.getBlockState(pos).getBlock() == BayouBluesBlocks.ALGAE.get()) {
+                            flag = true;
+                        }
+                    }
+                    if (flag) {
+                        worldIn.playSound((PlayerEntity) null, playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(), SoundEvents.ENTITY_FISHING_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (playerIn.getRNG().nextFloat() * 0.4F + 0.8F));
+                        if (!worldIn.isRemote) {
+                            int k = EnchantmentHelper.getFishingSpeedBonus(itemstack);
+                            int j = EnchantmentHelper.getFishingLuckBonus(itemstack) + 2;
+                            worldIn.addEntity(new FishingBobberEntity(playerIn, worldIn, j, k));
+                        }
+
+                        playerIn.addStat(Stats.ITEM_USED.get(itemstack.getItem()));
+
+                        event.setResult(Event.Result.ALLOW);
+                    }
+                }
             }
         }
     }
