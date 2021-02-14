6655ca122d309cdc96adb028fbaed04a54229154
Sarinsa <mikkemusbruker@gmail.com>
2021-02-14 12:19:27 +0100

    Added package-info files to die down a lot of annoying warnings.

---
+++ src/main/java/com/teamaurora/bayou_blues/common/item/package-info.java
@@ -0,0 +1,8 @@
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.teamaurora.bayou_blues.common.item;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ src/main/java/com/teamaurora/bayou_blues/common/world/gen/feature/package-info.java
@@ -0,0 +1,8 @@
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.teamaurora.bayou_blues.common.world.gen.feature;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
